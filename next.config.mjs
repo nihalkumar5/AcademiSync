27288bdad6159b69392127ac7962c38bd60b1aca
nihal kumar <kumarnihal829@gmail.com>
2026-08-24 01:06:45 +0530

    Hardcode Clerk Dev Keys in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_Z3JlYXQtbWFybGluLTc5OTcuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_SECRET_KEY: "sk_test_Jy7AD5HGNNYIkkIt7mv6tFKbRwjEjN5HMxMY2oQKq1",
+  },
 };
 
 export default nextConfig;
