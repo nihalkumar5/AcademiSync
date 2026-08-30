703fe7c3b7a07d92f026da20486766a799a33346
nihal kumar <kumarnihal829@gmail.com>
2026-08-30 11:19:44 +0530

    fix: transpile capacitor packages in next config to resolve chunk resolution errors

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_Z3JlYXQtbWFybGluLTc5OTcuY2xlcmsuYWNjb3VudHMuZGV2JA",
     CLERK_SECRET_KEY: "sk_test_Jy7AD5HGNNYIkkIt7mv6tFKbRwjEjN5HMxMY2oQKq1",
   },
+  transpilePackages: [
+    "@capacitor/core",
+    "@capacitor/splash-screen",
+    "@capacitor/share",
+    "@capacitor/local-notifications",
+    "@capacitor/push-notifications"
+  ],
 };
 
 export default nextConfig;
